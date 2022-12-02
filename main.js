79534c0da1e19308693424a3c4ae582174ef4eae
Edward Ibeh-Young <edward.ibehyoung@gmail.com>
2022-12-02 16:37:03 -0700

    moved college to static

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ setTimeout(() => {
 
 const images = [
   './IMAGES/KhanAcademy_Page.jpeg',
-  './IMAGES/TecCrunch_Page.jpeg',
+  './IMAGES/College site.jpeg',
   './IMAGES/Restaurant_Page.jpeg',
   './IMAGES/MarketTemplate_Page.jpeg',
   './IMAGES/RealState.jpeg'];
